c2381e85c42e9b0d538e94f402a7b2677e129ff7
BeniaminCazacu <beny.cazacu@gmail.com>
2020-04-08 21:19:55 +0300

    Button-OK

--- Cosulet_Cu_Flori/Cosulet_Cu_Flori/Form1.cs
+++ Cosulet_Cu_Flori/Cosulet_Cu_Flori/Form1.cs
@@ -16,5 +16,10 @@ namespace Cosulet_Cu_Flori
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
